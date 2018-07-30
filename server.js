eb7d978a99ffb06cb0b25d62e9be0ca0ee2c109a
leanfj <leanfj@hotmail.com>
2018-07-30 19:18:09 -0300

    Atualização de porta

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./config/express")();
 require("./config/database.js")(
   "mongodb://leanfj:ab12345@ds259241.mlab.com:59241/feedreaderapp"
 );
-http.createServer(app).listen(app.get("port"), () => {
+http.createServer(app).listen(process.env.PORT || app.get("port"), () => {
   console.log("Express Server escutando na porta " + app.get("port"));
 });
